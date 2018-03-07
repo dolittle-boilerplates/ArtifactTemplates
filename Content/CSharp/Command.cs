a2281e6eaa28452785d5bbd3e28c402201b6f5f6
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-07 09:56:05 +0100

    Added extension

---
+++ Content/CSharp/Command.cs
@@ -0,0 +1,12 @@
+// template: {
+//    "name": "command",
+//    "description": "Creates a command"
+// }
+using Dolittle.Commands;
+
+namespace %namespace%
+{
+  public class %name% : ICommand
+  {
+  }
+}
