9f42af1eabe62eed505ae504583fde07d0cba25d
Sindre Wilting <sindrewilting@gmail.com>
2018-11-08 14:03:08 +0100

    Automaticaly discoveres and adds aggregate root repos to the command handler artifact

--- Content/CSharp/CommandHandler/{{name}}.cs
+++ Content/CSharp/CommandHandler/{{name}}.cs
@@ -1,9 +1,27 @@
 using Dolittle.Commands.Handling;
-
+{{#if aggregateRoots}}
+using Dolittle.Domain;
+{{/if}}
 namespace {{namespace}}
 {
     public class {{name}} : ICanHandleCommands
     {
+        {{#if aggregateRoots}}
+        {{#each aggregateRoots}}
+        readonly IAggregateRootRepositoryFor<{{this}}>  _aggregateRootRepoFor{{this}};
+        {{/each}}
+
+        public {{name}}(
+            {{#each aggregateRoots}}{{#if @first}}IAggregateRootRepositoryFor<{{this}}>  aggregateRootRepoFor{{this}}{{#unless @last}},{{/unless}}{{/if}}            {{#if @last}}{{#unless @first}}IAggregateRootRepositoryFor<{{this}}>  aggregateRootRepoFor{{this}}{{/unless}}{{/if}}
+            {{/each}}
+        )
+        {
+            {{#each aggregateRoots}}
+             _aggregateRootRepoFor{{this}} =  aggregateRootRepoFor{{this}};
+            {{/each}}
+        }
+        {{/if}}
+
         {{#each commands}}
         public void Handle({{this}} cmd)
         {
